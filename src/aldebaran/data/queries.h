b5f99c0d93b15d6ee086db607f4fcf647844a9c4
santon <santon@bc7e9152-9d3f-0410-9ec8-84bf1179fcc2>
2011-05-29 13:06:33 +0000

    added missing files: aldeabran/data/queries.*

---
+++ src/aldebaran/data/queries.h
@@ -0,0 +1,22 @@
+#ifndef QUERIES_H
+#define QUERIES_H
+
+#include <qstring.h>
+
+class QTranslator;
+
+class Queries
+{
+public:
+    static void setDialect(QString dialect);
+    static QString tr(QString query);
+    static Queries * getQueries();
+protected:
+    Queries(QString dialect="qpsql7");
+    void init();
+    static Queries* instance;
+    QString fDialect;
+    QTranslator * trans;
+};
+
+#endif
