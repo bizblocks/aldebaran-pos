80147802bfff4e0dcff45442c79b6d6bd683c502
santon <santon@bc7e9152-9d3f-0410-9ec8-84bf1179fcc2>
2011-06-02 07:59:15 +0000

    Added missing dlecrsettings.* sources

---
+++ src/tradeequip/equip/dlgecrsettings.ui.h
@@ -0,0 +1,58 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you want to add, delete, or rename functions or slots, use
+** Qt Designer to update this file, preserving your code.
+**
+** You should not define a constructor or destructor in this file.
+** Instead, write your code in functions called init() and destroy().
+** These will automatically be called by the form's constructor and
+** destructor.
+*****************************************************************************/
+
+void dlgECRSettings::init( eqDriver * device )
+{
+    fDevice = (eqFR*)device;
+    if(!fDevice) return;
+    
+    QStringList drvList = fDevice->driverList();
+    for(int i=0;i<drvList.size();i++) drvList[i] = drvList[i].replace("TECashRegister", "");
+	
+    comboDriver->insertStringList(drvList);
+    comboBaudRate->insertStringList(fDevice->supportedBaudRates());
+    comboDataBits->insertStringList(fDevice->supportedDataBits());
+    comboParity->insertStringList(fDevice->supportedParity());
+    comboStopBits->insertStringList(fDevice->supportedStopBits());
+    comboFlowControl->insertStringList(fDevice->supportedFlowControl());
+    
+    editDevice->setText(fDevice->option("device"));
+    comboDriver->setCurrentText(fDevice->option("driver"));
+    comboBaudRate->setCurrentText(fDevice->option("baudrate"));
+    comboDataBits->setCurrentItem(fDevice->option("data bits").toInt());
+    comboParity->setCurrentItem(fDevice->option("parity").toInt());
+    comboStopBits->setCurrentItem(fDevice->option("stopbits").toInt());
+    comboFlowControl->setCurrentItem(fDevice->option("flow control").toInt());
+    
+    editAccessPassword->setText(fDevice->option("access password"));
+    editOperatorPassword->setText(fDevice->option("operator password"));
+    editAdminPassword->setText(fDevice->option("administrator password"));
+}
+
+void dlgECRSettings::apply()
+{
+    if(fDevice)
+    {
+	fDevice->setOption("driver", comboDriver->currentText());
+	fDevice->setOption("device", editDevice->text());
+	fDevice->setOption("baudrate", comboBaudRate->currentText());
+	fDevice->setOption("parity", comboParity->currentItem());
+	fDevice->setOption("data bits", comboDataBits->currentItem());
+	fDevice->setOption("stopbits", comboStopBits->currentItem());
+	fDevice->setOption("flow control", comboFlowControl->currentItem());
+	fDevice->setOption("access password", editAccessPassword->text());
+	fDevice->setOption("operator password", editOperatorPassword->text());
+	fDevice->setOption("administrator password", editAdminPassword->text());
+	accept();
+    }
+    else reject();
+}
